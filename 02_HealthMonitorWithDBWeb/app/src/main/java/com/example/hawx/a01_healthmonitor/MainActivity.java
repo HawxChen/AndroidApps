503a83f4f9e156e446c16f21e91b8ea176618692
Matt Borgerson <contact@mborgerson.com>
2017-10-01 22:32:18 -0700

    Fix db issues causing crashes

--- 02_HealthMonitorWithDBWeb/app/src/main/java/com/example/hawx/a01_healthmonitor/MainActivity.java
+++ 02_HealthMonitorWithDBWeb/app/src/main/java/com/example/hawx/a01_healthmonitor/MainActivity.java
@@ -138,7 +138,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         if(!mTableNameCurrentOpen.equals(mConcatName)) {
             sddb = sddbhelper.getWritableDatabase(mConcatName);
-            mTableNameCurrentOpen = mConcatName;
+            mTableNameCurrentOpen = "\"" + mConcatName + "\"";
             startAccSensService();
         }
 
--- 02_HealthMonitorWithDBWeb/app/src/main/java/com/example/hawx/a01_healthmonitor/SDSQLiteHelper.java
+++ 02_HealthMonitorWithDBWeb/app/src/main/java/com/example/hawx/a01_healthmonitor/SDSQLiteHelper.java
@@ -1,4 +1,5 @@
 package com.example.hawx.a01_healthmonitor;
+import android.database.Cursor;
 import android.os.Environment;
 import java.lang.String;
 import java.io.File;
@@ -12,14 +13,12 @@ import android.util.Log;
 //Reference: https://stackoverflow.com/questions/7229450/sqliteopenhelper-creating-database-on-sd-card
 public class SDSQLiteHelper {
     private static final String TAG  = "DatabaseHelper";
-    public static final File DB_FILE = Environment.getExternalStorageDirectory();
-    public static final String DB_FILE_PATH = DB_FILE.getAbsolutePath();
-    public static final String DB_NAME = "Group25.db";
     private String mTableNameLastOpen; //Take convenience of this project.
+    private String db_path;
     private SQLiteDatabase db;
 
     public static class SDSQLiteSchema {
-        public static final String INCREASE_ID = "0";
+        public static final String INCREASE_ID = "ID";
         public static final String TS_FIELD = "TIMESTAMP";
         public static final String X_FIELD = "X";
         public static final String Y_FIELD = "Y";
@@ -28,13 +27,18 @@ public class SDSQLiteHelper {
 
     public SDSQLiteHelper()
     {
+
+        File   DB_FILE      = Environment.getExternalStorageDirectory();
+        String DB_FILE_PATH = DB_FILE.getAbsolutePath();
+        String DB_NAME      = "Group25.db";
+
+        db_path = DB_FILE_PATH  + File.separator + DB_NAME;
+
         try
         {
             // Open the database (create if it does not exist)
-            Log.e(TAG,"DB Path : " + DB_FILE_PATH  + File.separator + DB_NAME);
-            db = SQLiteDatabase.openDatabase(DB_FILE_PATH + File.separator + DB_NAME,
-                                             null,
-                                             SQLiteDatabase.OPEN_READWRITE | SQLiteDatabase.CREATE_IF_NECESSARY);
+            Log.e(TAG,"DB Path: " + db_path);
+            db = SQLiteDatabase.openDatabase(db_path, null, SQLiteDatabase.OPEN_READWRITE | SQLiteDatabase.CREATE_IF_NECESSARY);
             Log.e(TAG, "Successful DB: " + db.toString());
         }
         catch (SQLiteException ex)
@@ -44,8 +48,8 @@ public class SDSQLiteHelper {
         }
         finally
         {
-            SQLiteDatabase.deleteDatabase(new File(DB_FILE_PATH
-                    + File.separator + DB_NAME));
+            //SQLiteDatabase.deleteDatabase(new File(DB_FILE_PATH
+            //        + File.separator + DB_NAME));
         }
     }
 
@@ -55,15 +59,18 @@ public class SDSQLiteHelper {
         //https://sqlite.org/autoinc.html
         //https://www.techonthenet.com/sql/tables/create_table.php
         mTableNameLastOpen = tableName;
-        String CMD = "CREATE TABLE " + tableName + " ( "
+
+        String CMD = "CREATE TABLE IF NOT EXISTS \"" + tableName + "\" ( "
                 + SDSQLiteSchema.INCREASE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + SDSQLiteSchema.TS_FIELD + " INTEGER NOT NULL, "
                 + SDSQLiteSchema.X_FIELD + " REAL NOT NULL, "
                 + SDSQLiteSchema.Y_FIELD + " REAL NOT NULL, "
                 + SDSQLiteSchema.Z_FIELD + " REAL NOT NULL " + "); ";
 
-        Log.e(TAG, "Creating Table: " + db.toString());
+        Log.e(TAG, "Creating Table in datbase " + db.toString());
+        Log.e(TAG, "SQL Query: " + CMD);
         db.execSQL(CMD);
+        Log.e(TAG, "Success");
     }
 
     public void closeDB()
@@ -73,16 +80,11 @@ public class SDSQLiteHelper {
 
     public static void deleteDB()
     {
-        SQLiteDatabase.deleteDatabase(new File(DB_FILE_PATH
-                + File.separator + DB_NAME));
+        //SQLiteDatabase.deleteDatabase(new File(db_path));
     }
 
     public SQLiteDatabase getWritableDatabase(String tableName)
     {
-        mTableNameLastOpen = tableName;
-        db = SQLiteDatabase.openDatabase(DB_FILE_PATH
-                        + File.separator + tableName, null,
-                SQLiteDatabase.OPEN_READWRITE);
         return db;
     }
 
