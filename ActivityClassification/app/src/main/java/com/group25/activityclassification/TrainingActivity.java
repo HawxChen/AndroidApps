ea60a332a88f48b94f20da4b56e803e60ded7cb1
Matt Borgerson <contact@mborgerson.com>
2017-11-23 19:53:21 -0700

    Implement guided data collection

--- ActivityClassification/app/src/main/java/com/group25/activityclassification/TrainingActivity.java
+++ ActivityClassification/app/src/main/java/com/group25/activityclassification/TrainingActivity.java
@@ -10,8 +10,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -41,16 +39,18 @@ public class TrainingActivity extends AppCompatActivity implements View.OnClickL
     private TextView       mAccelerometerLiveData;
     private Button         mStartButton;
     private SensorManager  mSensorMgr;
-    private RadioGroup     mActivityGroup;
-    private TextView       mWalkingActivityCount;
-    private TextView       mRunningActivityCount;
-    private TextView       mJumpingActivityCount;
+    private TextView       mActivityTextView;
+    private TextView       mTimerTextView;
 
     // State tracking
     private Boolean        mIsRunning;
     private Boolean        mIsAccelerometerRegistered;
     private ActivityType   mActivity;
     private CountDownTimer mCountDownTimer;
+    private CountDownTimer mActivityTimer;
+    private int            countdownInterval;
+    private int            collectionInterval;
+    private int            mState;
 
     // Sample database
     private ArrayList<AccelerometerSample> mWalkingSamples;
@@ -66,11 +66,9 @@ public class TrainingActivity extends AppCompatActivity implements View.OnClickL
         mStartButton = (Button) findViewById(R.id.startButton);
         mStartButton.setOnClickListener(this);
 
-//        mActivityGroup = (RadioGroup) findViewById(R.id.activtiyGroup);
         mAccelerometerLiveData = (TextView) findViewById(R.id.accelerometerLiveData);
-//        mWalkingActivityCount = (TextView) findViewById(R.id.walkingActivityCount);
-//        mRunningActivityCount = (TextView) findViewById(R.id.runningActivityCount);
-//        mJumpingActivityCount = (TextView) findViewById(R.id.jumpingActivityCount);
+        mActivityTextView = (TextView) findViewById(R.id.activityTextView);
+        mTimerTextView = (TextView) findViewById(R.id.timerTextView);
 
         mSensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
         mIsAccelerometerRegistered = false;
@@ -78,6 +76,10 @@ public class TrainingActivity extends AppCompatActivity implements View.OnClickL
         mWalkingSamples = new ArrayList<AccelerometerSample>();
         mRunningSamples = new ArrayList<AccelerometerSample>();
         mJumpingSamples = new ArrayList<AccelerometerSample>();
+
+        mState = 0;
+        countdownInterval = 5;
+        collectionInterval = 10;
 
         stop();
     }
@@ -93,66 +95,6 @@ public class TrainingActivity extends AppCompatActivity implements View.OnClickL
             actionBar.setHomeButtonEnabled(true);
             actionBar.setTitle("Training");
         }
-    }
-
-    //
-    // Helper method to enable or disable all of the activity options
-    //
-    private void setRadioGroupEnabled(Boolean enabled) {
-//        ((RadioButton)(findViewById(R.id.walkingActivity))).setEnabled(enabled);
-//        ((RadioButton)(findViewById(R.id.runningActivity))).setEnabled(enabled);
-//        ((RadioButton)(findViewById(R.id.jumpingActivity))).setEnabled(enabled);
-    }
-
-    //
-    // Start sample collection (Start with countdown to allow user time to begin activity)
-    //
-    private void start() {
-        mStartButton.setText("Stop");
-        mIsRunning = true;
-        mActivity = ActivityType.ACTIVITY_RUNNING;
-//        setRadioGroupEnabled(false);
-//        switch (mActivityGroup.getCheckedRadioButtonId()) {
-//            case R.id.walkingActivity: mActivity = ActivityType.ACTIVITY_WALKING; break;
-//            case R.id.runningActivity: mActivity = ActivityType.ACTIVITY_RUNNING; break;
-//            case R.id.jumpingActivity: mActivity = ActivityType.ACTIVITY_JUMPING; break;
-//            default:                   mActivity = ActivityType.ACTIVITY_UNKNOWN; break;
-//        }
-        mCountDownTimer = new CountDownTimer(5000, 1000) {
-            public void onTick(long millisUntilFinished) {
-                mAccelerometerLiveData.setText(String.format("Please begin activity!\n\nRecording starting in %d...", millisUntilFinished / 1000 + 1));
-            }
-
-            public void onFinish() {
-                start_recording_activity();
-            }
-        }.start();
-    }
-
-    private void start_recording_activity() {
-        if (!mIsAccelerometerRegistered) {
-            Sensor accelSensor = mSensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-            mSensorMgr.registerListener(this, accelSensor, 100000); // Every 100ms (10Hz)
-            mIsAccelerometerRegistered = true;
-        }
-    }
-
-    //
-    // Stop Sample Collection
-    //
-    private void stop() {
-        if (mIsAccelerometerRegistered) {
-            mSensorMgr.unregisterListener(this);
-            mIsAccelerometerRegistered = false;
-        }
-        if (mCountDownTimer != null) {
-            mCountDownTimer.cancel();
-            mCountDownTimer = null;
-        }
-        mIsRunning = false;
-        mStartButton.setText("Start");
-        mAccelerometerLiveData.setText("Press \"Start\" button to begin collecting samples.");
-//        setRadioGroupEnabled(true);
     }
 
     //
@@ -175,6 +117,119 @@ public class TrainingActivity extends AppCompatActivity implements View.OnClickL
     }
 
     //
+    // Start sample collection
+    //
+    private void start() {
+        mStartButton.setText("Stop");
+        mIsRunning = true;
+        mState = 0;
+        beginActivity();
+    }
+
+    //
+    // Stop activity state machine
+    //
+    private void stop() {
+        stopSampling();
+        if (mCountDownTimer != null) {
+            mCountDownTimer.cancel();
+            mCountDownTimer = null;
+        }
+        if (mActivityTimer != null) {
+            mActivityTimer.cancel();
+            mActivityTimer = null;
+        }
+        mIsRunning = false;
+        mStartButton.setText("Start");
+        mAccelerometerLiveData.setText("Press \"Start\" button to begin collecting samples.");
+        mTimerTextView.setText("");
+        mActivityTextView.setText("");
+    }
+
+    //
+    // Activity state machine
+    //
+    private void beginActivity() {
+        switch (mState) {
+        case 0:
+            mActivity = ActivityType.ACTIVITY_WALKING;
+            mActivityTextView.setText("Walking");
+            break;
+        case 1:
+            mActivity = ActivityType.ACTIVITY_RUNNING;
+            mActivityTextView.setText("Running");
+            break;
+        case 2:
+            mActivity = ActivityType.ACTIVITY_JUMPING;
+            mActivityTextView.setText("Jumping");
+            break;
+        case 3:
+            stop();
+            return;
+        }
+
+        mState += 1;
+        startActivityRecording();
+    }
+
+    //
+    // Start sampling
+    //
+    private void startActivityRecording() {
+        mAccelerometerLiveData.setText("Please begin activity!");
+
+        // Create a countdown timer to time activity recording
+        mActivityTimer = new CountDownTimer(collectionInterval * 1000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                mTimerTextView.setText(String.format("%d seconds remaining", millisUntilFinished / 1000 + 1));
+            }
+
+            public void onFinish() {
+                mAccelerometerLiveData.setText("");
+                mTimerTextView.setText("");
+                stopSampling();
+
+                // Begin next activity in state machine
+                beginActivity();
+            }
+        };
+
+        // Create a countdown timer to allow the user to begin activity
+        mCountDownTimer = new CountDownTimer(countdownInterval * 1000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                mTimerTextView.setText(String.format("Recording starting in %d", millisUntilFinished / 1000 + 1));
+            }
+
+            public void onFinish() {
+                mTimerTextView.setText("");
+                mActivityTimer.start();
+                startSampling();
+            }
+        }.start();
+    }
+
+    //
+    // Start sampling
+    //
+    private void startSampling() {
+        if (!mIsAccelerometerRegistered) {
+            Sensor accelSensor = mSensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            mSensorMgr.registerListener(this, accelSensor, 100000); // Every 100ms (10Hz)
+            mIsAccelerometerRegistered = true;
+        }
+    }
+
+    //
+    // Stop sampling
+    //
+    private void stopSampling() {
+        if (mIsAccelerometerRegistered) {
+            mSensorMgr.unregisterListener(this);
+            mIsAccelerometerRegistered = false;
+        }
+    }
+
+    //
     // Accelerometer Data Callback
     //
     @Override
@@ -188,18 +243,16 @@ public class TrainingActivity extends AppCompatActivity implements View.OnClickL
         switch (mActivity) {
             case ACTIVITY_WALKING:
                 mWalkingSamples.add(new AccelerometerSample(x, y, z));
-//                mWalkingActivityCount.setText(String.format("%d", mWalkingSamples.size()));
                 break;
             case ACTIVITY_RUNNING:
                 mRunningSamples.add(new AccelerometerSample(x, y, z));
-//                mRunningActivityCount.setText(String.format("%d", mRunningSamples.size()));
                 break;
             case ACTIVITY_JUMPING:
                 mJumpingSamples.add(new AccelerometerSample(x, y, z));
-//                mJumpingActivityCount.setText(String.format("%d", mJumpingSamples.size()));
                 break;
         }
 
+        // Update accelerometer live text view
         mAccelerometerLiveData.setText(String.format("X: %f, Y: %f; Z: %f", x, y, z));
     }
 
